e75e8dd0a474d0a495353eb88115288b6ed779cf
robert trebor <treborx555@gmail.com>
2019-04-25 13:37:00 +0200

    blocks done i think

--- src/main/java/com/robertx22/advanced_blocks/BaseInventoryBlock.java
+++ src/main/java/com/robertx22/advanced_blocks/BaseInventoryBlock.java
@@ -1,18 +1,12 @@
 package com.robertx22.advanced_blocks;
 
-import com.robertx22.mmorpg.Main;
-import com.robertx22.mmorpg.gui.GuiHandler;
-
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
@@ -25,16 +19,6 @@ public abstract class BaseInventoryBlock extends BlockContainer {
     protected BaseInventoryBlock(Properties prop) {
 	super(prop);
 
-    }
-
-    @Override
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
-	    EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-	if (worldIn.isRemote)
-	    return true;
-
-	playerIn.openGui(Main.instance, GuiHandler.getGuiID(), worldIn, pos.getX(), pos.getY(), pos.getZ());
-	return true;
     }
 
     @Override
--- src/main/java/com/robertx22/advanced_blocks/repair_station/BlockInventoryRepair.java
+++ src/main/java/com/robertx22/advanced_blocks/repair_station/BlockInventoryRepair.java
@@ -3,8 +3,14 @@ package com.robertx22.advanced_blocks.repair_station;
 import com.robertx22.advanced_blocks.BaseInventoryBlock;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 public class BlockInventoryRepair extends BaseInventoryBlock {
 
@@ -15,6 +21,21 @@ public class BlockInventoryRepair extends BaseInventoryBlock {
     @Override
     public TileEntity createNewTileEntity(IBlockReader worldIn) {
 	return new TileInventoryRepair();
+    }
+
+    @Override
+    public boolean onBlockActivated(IBlockState state, World world, BlockPos pos, EntityPlayer player, EnumHand hand,
+	    EnumFacing side, float hitX, float hitY, float hitZ) {
+	if (world.isRemote)
+	    return true;
+
+	TileEntity tile = world.getTileEntity(pos);
+
+	if (tile instanceof TileInventoryRepair) {
+	    player.displayGui((TileInventoryRepair) tile);
+	}
+
+	return true;
     }
 
 }
