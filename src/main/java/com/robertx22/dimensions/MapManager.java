8f662408bd4ba855d23fffbdca871bdf5839f39a
robert trebor <treborx555@gmail.com>
2019-04-25 16:37:53 +0200

    the hard part now

---
+++ src/main/java/com/robertx22/dimensions/MapManager.java
@@ -0,0 +1,28 @@
+package com.robertx22.dimensions;
+
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.common.ModDimension;
+
+public class MapManager {
+
+    public static boolean isRegistered(ResourceLocation res) {
+
+	return DimensionType.byName(res) != null;
+    }
+
+    public static void register(ResourceLocation res, ModDimension moddim) {
+
+	DimensionManager.registerDimension(res, moddim, new PacketBuffer(Unpooled.wrappedBuffer(new byte[] {})));
+    }
+
+    public static void unRegister(DimensionType type) {
+
+	DimensionManager.unregisterDimension(type.getId());
+
+    }
+
+}
