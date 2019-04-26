307151eb04232e39e3aa3a56aaa9c97a564c1dee
robert trebor <treborx555@gmail.com>
2019-04-26 18:33:27 +0200

    should work i hope

--- src/main/java/com/robertx22/uncommon/gui/dmg_numbers/OnDisplayDamage.java
+++ src/main/java/com/robertx22/uncommon/gui/dmg_numbers/OnDisplayDamage.java
@@ -36,13 +36,8 @@ public class OnDisplayDamage {
 	Particle damageIndicator = new DamageParticle(Elements.byNumber(data.element), data.string, world, data.x,
 		data.y + data.height, data.z, motionX, motionY, motionZ);
 
-	// mc.particles.addParticle(particleData, x, y, z, xSpeed, ySpeed, zSpeed)
-	// Minecraft.getInstance().world(damageIndicator, false, data.x, data.y +
-	// data.y, data.z, 1D, 1D, 1D); // UNSURE
-	// IF
-	// WORKS
-	// COULD
-	// ERROR
+	Minecraft.getInstance().particles.addEffect(damageIndicator);
+
     }
 
     @Nullable
