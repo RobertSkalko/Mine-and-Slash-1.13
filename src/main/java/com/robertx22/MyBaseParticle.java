7aa89fdcf8bf2cfa210c75e7e1de264aae7848db
robert trebor <treborx555@gmail.com>
2019-05-20 19:11:15 +0200

    backup

--- src/main/java/com/robertx22/MyBaseParticle.java
+++ src/main/java/com/robertx22/MyBaseParticle.java
@@ -2,11 +2,7 @@ package com.robertx22;
 
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
@@ -19,37 +15,21 @@ import java.util.Random;
 @OnlyIn(Dist.CLIENT)
 public abstract class MyBaseParticle extends Particle {
     private static final Random RANDOM = new Random();
-
     private final TextureManager textureManager;
 
     public abstract ResourceLocation particleTexutre();
 
-    private final int lifeTime;
-    private final float size = 0.1F;
-    private int life;
+    //  private static final VertexFormat VERTEX_FORMAT = (new VertexFormat()).addElement(DefaultVertexFormats.POSITION_3F).addElement(DefaultVertexFormats.TEX_2F).addElement(DefaultVertexFormats.COLOR_4UB).addElement(DefaultVertexFormats.TEX_2S).addElement(DefaultVertexFormats.NORMAL_3B).addElement(DefaultVertexFormats.PADDING_1B);
+    //
 
-    private static final VertexFormat VERTEX_FORMAT = (new VertexFormat()).addElement(DefaultVertexFormats.POSITION_3F)
-            .addElement(DefaultVertexFormats.TEX_2F)
-            .addElement(DefaultVertexFormats.COLOR_4UB)
-            .addElement(DefaultVertexFormats.TEX_2S)
-            .addElement(DefaultVertexFormats.NORMAL_3B)
-            .addElement(DefaultVertexFormats.PADDING_1B);
-
-    /**
-     * Base spell texture index
-     */
-    private int baseSpellTextureIndex = 128;
+    private static final VertexFormat VERTEX_FORMAT = new VertexFormat();
 
     protected MyBaseParticle(TextureManager textureManagerIn, World worldIn,
                              double xCoordIn, double yCoordIn, double zCoordIn,
                              double xSpeed, double ySpeed, double zSpeed) {
         super(worldIn, xCoordIn, yCoordIn, zCoordIn, 0.0D, 0.0D, 0.0D);
         this.textureManager = textureManagerIn;
-        this.lifeTime = 6 + this.rand.nextInt(4);
-        float f = this.rand.nextFloat() * 0.6F + 0.4F;
-        this.particleRed = f;
-        this.particleGreen = f;
-        this.particleBlue = f;
+        this.setColor(1.0F, 1.0F, 1.0F);
 
     }
 
@@ -57,64 +37,15 @@ public abstract class MyBaseParticle extends Particle {
     public void renderParticle(BufferBuilder buffer, Entity entityIn, float partialTicks,
                                float rotationX, float rotationZ, float rotationYZ,
                                float rotationXY, float rotationXZ) {
-        int i = (int) (((float) this.life + partialTicks) * 15.0F / (float) this.lifeTime);
-        if (i <= 15) {
-            this.textureManager.bindTexture(this.particleTexutre());
-            float f = (float) (i % 4) / 4.0F;
-            float f1 = f + 0.24975F;
-            float f2 = (float) (i / 4) / 4.0F;
-            float f3 = f2 + 0.24975F;
-            float f4 = 2.0F * this.size;
-            float f5 = (float) (this.prevPosX + (this.posX - this.prevPosX) * (double) partialTicks - interpPosX);
-            float f6 = (float) (this.prevPosY + (this.posY - this.prevPosY) * (double) partialTicks - interpPosY);
-            float f7 = (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * (double) partialTicks - interpPosZ);
-            GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-            GlStateManager.disableLighting();
-            RenderHelper.disableStandardItemLighting();
-            buffer.begin(7, VERTEX_FORMAT);
-            buffer.pos((double) (f5 - rotationX * f4 - rotationXY * f4), (double) (f6 - rotationZ * f4), (double) (f7 - rotationYZ * f4 - rotationXZ * f4))
-                    .tex((double) f1, (double) f3)
-                    .color(this.particleRed, this.particleGreen, this.particleBlue, 1.0F)
-                    .lightmap(0, 240)
-                    .normal(0.0F, 1.0F, 0.0F)
-                    .endVertex();
-            buffer.pos((double) (f5 - rotationX * f4 + rotationXY * f4), (double) (f6 + rotationZ * f4), (double) (f7 - rotationYZ * f4 + rotationXZ * f4))
-                    .tex((double) f1, (double) f2)
-                    .color(this.particleRed, this.particleGreen, this.particleBlue, 1.0F)
-                    .lightmap(0, 240)
-                    .normal(0.0F, 1.0F, 0.0F)
-                    .endVertex();
-            buffer.pos((double) (f5 + rotationX * f4 + rotationXY * f4), (double) (f6 + rotationZ * f4), (double) (f7 + rotationYZ * f4 + rotationXZ * f4))
-                    .tex((double) f, (double) f2)
-                    .color(this.particleRed, this.particleGreen, this.particleBlue, 1.0F)
-                    .lightmap(0, 240)
-                    .normal(0.0F, 1.0F, 0.0F)
-                    .endVertex();
-            buffer.pos((double) (f5 + rotationX * f4 - rotationXY * f4), (double) (f6 - rotationZ * f4), (double) (f7 + rotationYZ * f4 - rotationXZ * f4))
-                    .tex((double) f, (double) f3)
-                    .color(this.particleRed, this.particleGreen, this.particleBlue, 1.0F)
-                    .lightmap(0, 240)
-                    .normal(0.0F, 1.0F, 0.0F)
-                    .endVertex();
-            Tessellator.getInstance().draw();
-            GlStateManager.enableLighting();
-        }
+
+        this.textureManager.bindTexture(this.particleTexutre());
+        super.renderParticle(buffer, entityIn, partialTicks, rotationX, rotationZ, rotationYZ, rotationXY, rotationXZ);
+
     }
 
     @Override
     public int getFXLayer() {
         return 3;
-    }
-
-    public void tick() {
-        this.prevPosX = this.posX;
-        this.prevPosY = this.posY;
-        this.prevPosZ = this.posZ;
-        ++this.life;
-        if (this.life == this.lifeTime) {
-            this.setExpired();
-        }
-
     }
 
 }
--- src/main/java/com/robertx22/WaterParticle.java
+++ src/main/java/com/robertx22/WaterParticle.java
@@ -1,16 +1,13 @@
 package com.robertx22;
 
 import com.robertx22.mmorpg.Ref;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.particle.IParticleFactory;
-import net.minecraft.client.particle.Particle;
 import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.particles.BasicParticleType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 public class WaterParticle extends MyBaseParticle {
 
     private static final ResourceLocation PARTICLE_TEXTURE = new ResourceLocation(Ref.MODID, "textures/particles/water.png");
@@ -26,14 +23,4 @@ public class WaterParticle extends MyBaseParticle {
         return PARTICLE_TEXTURE;
     }
 
-    @OnlyIn(Dist.CLIENT)
-    public static class Factory implements IParticleFactory<BasicParticleType> {
-        @Override
-        public Particle makeParticle(BasicParticleType typeIn, World worldIn, double x,
-                                     double y, double z, double xSpeed, double ySpeed,
-                                     double zSpeed) {
-            return new WaterParticle(Minecraft.getInstance()
-                    .getTextureManager(), worldIn, x, y, z, xSpeed, ySpeed, zSpeed);
-        }
-    }
 }
