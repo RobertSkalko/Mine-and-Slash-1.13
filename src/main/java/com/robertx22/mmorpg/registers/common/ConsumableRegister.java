1448d7aee671460e7de2abc81769c57e78f6f9a1
robert trebor <treborx555@gmail.com>
2019-05-23 09:06:08 +0200

    updated reg

--- src/main/java/com/robertx22/mmorpg/registers/common/ConsumableRegister.java
+++ src/main/java/com/robertx22/mmorpg/registers/common/ConsumableRegister.java
@@ -14,8 +14,8 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod.EventBusSubscriber(modid = Ref.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ConsumableRegister {
 
-    public static final String RESTORE_MANA_ID = Ref.MODID + ":restore_mana_item";
-    public static final String RESTORE_ENERGY_ID = Ref.MODID + ":restore_energy_item";
+    public static final String RESTORE_MANA_ID = Ref.MODID + ":consumable/restore_mana_item";
+    public static final String RESTORE_ENERGY_ID = Ref.MODID + ":consumable/restore_energy_item";
 
     @ObjectHolder(RESTORE_MANA_ID)
     public static Block RESTORE_MANA;
