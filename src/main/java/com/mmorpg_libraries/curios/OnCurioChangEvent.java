722277483034357e65c4c15a094535b9368be9d2
robert trebor <treborx555@gmail.com>
2019-06-01 18:09:45 +0200

    huh curios has equip change event too!

--- src/main/java/com/mmorpg_libraries/curios/OnCurioChangEvent.java
+++ src/main/java/com/mmorpg_libraries/curios/OnCurioChangEvent.java
@@ -1,25 +1,23 @@
-package com.robertx22.onevent.entity;
+package com.mmorpg_libraries.curios;
 
 import com.robertx22.uncommon.capability.EntityData;
 import com.robertx22.uncommon.datasaving.Load;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import top.theillusivec4.curios.api.event.LivingCurioChangeEvent;
 
 @Mod.EventBusSubscriber
-public class OnEquipChange {
-
-    // on change, notify my capability so i know stats need to be recalculated
+public class OnCurioChangEvent {
     @SubscribeEvent
-    public static void onEquipChange(LivingEquipmentChangeEvent event) {
+    public static void onEquipCurioChange(LivingCurioChangeEvent event) {
 
         EntityLivingBase entity = event.getEntityLiving();
         if (entity != null) {
             EntityData.UnitData data = Load.Unit(entity);
             data.setEquipsChanged(true);
             data.recalculateStats(entity);
+
         }
     }
-
 }
