6b5e285145cb38b60a83ff9e8b9ba743e436ad9f
robert trebor <treborx555@gmail.com>
2019-06-09 17:44:15 +0200

    aaaa

--- src/main/java/com/robertx22/mmorpg/registers/client/OnKeyPress.java
+++ src/main/java/com/robertx22/mmorpg/registers/client/OnKeyPress.java
@@ -2,6 +2,7 @@ package com.robertx22.mmorpg.registers.client;
 
 import com.robertx22.uncommon.gui.StatGUI;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,10 +10,13 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class OnKeyPress {
 
+    // they said i should use clienttickevent but idk how
     @SubscribeEvent
     public static void onKeyInput(InputEvent.KeyInputEvent event) {
 
         int key = event.getKey();
+
+        PlayerEntity player = Minecraft.getInstance().player;
 
         if (Minecraft.getInstance().field_71462_r == null) {
 
