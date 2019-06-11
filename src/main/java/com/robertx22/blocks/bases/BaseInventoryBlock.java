f2e69d2b2d98fa0d0d88c836f396e25358aa18d7
robert trebor <treborx555@gmail.com>
2019-06-11 16:41:22 +0200

    nice block drops work

--- src/main/java/com/robertx22/blocks/bases/BaseInventoryBlock.java
+++ src/main/java/com/robertx22/blocks/bases/BaseInventoryBlock.java
@@ -3,21 +3,18 @@ package com.robertx22.blocks.bases;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.Explosion;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.storage.loot.LootParameters;
 import net.minecraftforge.fml.network.NetworkHooks;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BaseInventoryBlock extends NonFullBlock {
@@ -27,25 +24,26 @@ public abstract class BaseInventoryBlock extends NonFullBlock {
     }
 
     @Deprecated
-    public List<ItemStack> getDrops(BlockState p_220076_1_,
-                                    LootContext.Builder p_220076_2_) {
-        return Arrays.asList(new ItemStack(this));
-    }
+    public List<ItemStack> getDrops(BlockState blockstate, LootContext.Builder context) {
 
-    @Override
-    public void onPlayerDestroy(IWorld worldIn, BlockPos pos, BlockState state) {
-        TileEntity tileEntity = worldIn.getTileEntity(pos);
-        if (tileEntity instanceof IInventory) {
-            InventoryHelper.dropInventoryItems((World) worldIn, pos, (IInventory) tileEntity);
-        }
-    }
+        ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+        items.add(new ItemStack(this));
 
-    @Override
-    public void onExplosionDestroy(World worldIn, BlockPos pos, Explosion explosionIn) {
-        TileEntity tileEntity = worldIn.getTileEntity(pos);
-        if (tileEntity instanceof IInventory) {
-            InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory) tileEntity);
+        TileEntity tileentity = context.get(LootParameters.BLOCK_ENTITY);
+
+        if (tileentity instanceof BaseTile) {
+
+            BaseTile inv = (BaseTile) tileentity;
+
+            for (ItemStack stack : inv.itemStacks) {
+                if (stack.isEmpty() == false) {
+                    items.add(stack);
+                }
+            }
+
         }
+
+        return items;
     }
 
     @Override
