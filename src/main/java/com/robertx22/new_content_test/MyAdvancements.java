184746056891fd12942e5b0b1db1ea6606fd362f
treborx555@gmail.com <treborx555@gmail.com>
2019-06-30 21:19:14 +0200

    hm they dont load

--- src/main/java/com/robertx22/new_content_test/MyAdvancements.java
+++ src/main/java/com/robertx22/new_content_test/MyAdvancements.java
@@ -12,20 +12,20 @@ import java.util.function.Consumer;
 public class MyAdvancements implements Consumer<Consumer<Advancement>> {
     public void accept(Consumer<Advancement> consumerAdv) {
         Advancement advancement = Advancement.Builder.builder()
-                .withDisplay(BlockRegister.BLOCK_GEAR_REPAIR, trans("repair.title"), trans("repair.description"), new ResourceLocation("textures/gui/advancements/backgrounds/end.png"), FrameType.TASK, false, false, false)
+                .withDisplay(BlockRegister.BLOCK_GEAR_REPAIR, trans("root.title"), trans("repair.description"), new ResourceLocation("textures/gui/advancements/backgrounds/end.png"), FrameType.TASK, false, false, false)
                 .withCriterion("placed_repair", PlacedBlockTrigger.Instance.placedBlock(BlockRegister.BLOCK_GEAR_REPAIR))
-                .register(consumerAdv, "/repair");
+                .register(consumerAdv, "mine_and_slash/root");
 
         Advancement advancement1 = Advancement.Builder.builder()
                 .withParent(advancement)
                 .withDisplay(BlockRegister.BLOCK_GEAR_SALVAGE, trans("salvage.title"), trans("salvage.description"), (ResourceLocation) null, FrameType.TASK, true, true, false)
                 .withCriterion("placed_salvage", PlacedBlockTrigger.Instance.placedBlock(BlockRegister.BLOCK_GEAR_SALVAGE))
-                .register(consumerAdv, "/salvage");
+                .register(consumerAdv, "mine_and_slash/salvage");
 
     }
-
+    
     private TranslationTextComponent trans(String str) {
-        return new TranslationTextComponent(".advancements.");
+        return new TranslationTextComponent("mmorpg.advancements." + str);
     }
 }
 
