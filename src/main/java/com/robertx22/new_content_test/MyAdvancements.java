20ccb8b63ddae8f3e4a32e86941ff8d894e388b5
treborx555@gmail.com <treborx555@gmail.com>
2019-06-30 21:34:56 +0200

    advacements work! now to improve them

--- src/main/java/com/robertx22/new_content_test/MyAdvancements.java
+++ src/main/java/com/robertx22/new_content_test/MyAdvancements.java
@@ -1,5 +1,6 @@
 package com.robertx22.new_content_test;
 
+import com.robertx22.mmorpg.Ref;
 import com.robertx22.mmorpg.registers.common.BlockRegister;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.FrameType;
@@ -10,22 +11,28 @@ import net.minecraft.util.text.TranslationTextComponent;
 import java.util.function.Consumer;
 
 public class MyAdvancements implements Consumer<Consumer<Advancement>> {
+
     public void accept(Consumer<Advancement> consumerAdv) {
         Advancement advancement = Advancement.Builder.builder()
                 .withDisplay(BlockRegister.BLOCK_GEAR_REPAIR, trans("root.title"), trans("repair.description"), new ResourceLocation("textures/gui/advancements/backgrounds/end.png"), FrameType.TASK, false, false, false)
                 .withCriterion("placed_repair", PlacedBlockTrigger.Instance.placedBlock(BlockRegister.BLOCK_GEAR_REPAIR))
-                .register(consumerAdv, "mine_and_slash/root");
+                .register(consumerAdv, id("root"));
 
         Advancement advancement1 = Advancement.Builder.builder()
                 .withParent(advancement)
                 .withDisplay(BlockRegister.BLOCK_GEAR_SALVAGE, trans("salvage.title"), trans("salvage.description"), (ResourceLocation) null, FrameType.TASK, true, true, false)
                 .withCriterion("placed_salvage", PlacedBlockTrigger.Instance.placedBlock(BlockRegister.BLOCK_GEAR_SALVAGE))
-                .register(consumerAdv, "mine_and_slash/salvage");
+                .register(consumerAdv, id("salvage"));
 
     }
     
     private TranslationTextComponent trans(String str) {
         return new TranslationTextComponent("mmorpg.advancements." + str);
     }
+
+    private String id(String str) {
+        return Ref.MODID + ":mine_and_slash/" + str;
+    }
+
 }
 
