baa04fff3b2bd54a798e24536ae54ce5f120d7c7
treborx555@gmail.com <treborx555@gmail.com>
2019-06-30 22:08:12 +0200

    reworked pngs, adding advacements

--- src/main/java/com/robertx22/new_content_test/MyAdvancements.java
+++ src/main/java/com/robertx22/new_content_test/MyAdvancements.java
@@ -4,7 +4,9 @@ import com.robertx22.mmorpg.Ref;
 import com.robertx22.mmorpg.registers.common.BlockRegister;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.FrameType;
-import net.minecraft.advancements.criterion.PlacedBlockTrigger;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TranslationTextComponent;
 
@@ -13,19 +15,27 @@ import java.util.function.Consumer;
 public class MyAdvancements implements Consumer<Consumer<Advancement>> {
 
     public void accept(Consumer<Advancement> consumerAdv) {
-        Advancement advancement = Advancement.Builder.builder()
-                .withDisplay(BlockRegister.BLOCK_GEAR_REPAIR, trans("root.title"), trans("repair.description"), new ResourceLocation("textures/gui/advancements/backgrounds/end.png"), FrameType.TASK, false, false, false)
-                .withCriterion("placed_repair", PlacedBlockTrigger.Instance.placedBlock(BlockRegister.BLOCK_GEAR_REPAIR))
+        Advancement parent = Advancement.Builder.builder()
+                .withDisplay(BlockRegister.BLOCK_MAP_DEVICE, trans("root.title"), trans("root.description"), new ResourceLocation("textures/gui/advancements/backgrounds/end.png"), FrameType.TASK, false, false, false)
+                .withCriterion("crafting_table", InventoryChangeTrigger.Instance.forItems(Blocks.CRAFTING_TABLE))
                 .register(consumerAdv, id("root"));
 
-        Advancement advancement1 = Advancement.Builder.builder()
-                .withParent(advancement)
-                .withDisplay(BlockRegister.BLOCK_GEAR_SALVAGE, trans("salvage.title"), trans("salvage.description"), (ResourceLocation) null, FrameType.TASK, true, true, false)
-                .withCriterion("placed_salvage", PlacedBlockTrigger.Instance.placedBlock(BlockRegister.BLOCK_GEAR_SALVAGE))
-                .register(consumerAdv, id("salvage"));
+        Advancement repair = createStationAdv(BlockRegister.BLOCK_GEAR_REPAIR, "repair", parent, consumerAdv);
+        Advancement modify = createStationAdv(BlockRegister.BLOCK_GEAR_MODIFY, "modify", parent, consumerAdv);
+        Advancement salvage = createStationAdv(BlockRegister.BLOCK_GEAR_SALVAGE, "salvage", parent, consumerAdv);
+        Advancement factory = createStationAdv(BlockRegister.BLOCK_GEAR_FACTORY, "factory", parent, consumerAdv);
 
     }
     
+    private Advancement createStationAdv(Block block, String id, Advancement parent,
+                                         Consumer<Advancement> consumerAdv) {
+        return Advancement.Builder.builder()
+                .withParent(parent)
+                .withDisplay(block, trans(id + ".title"), trans(id + ".description"), null, FrameType.TASK, true, true, false)
+                .withCriterion("placed_" + id, InventoryChangeTrigger.Instance.forItems(block))
+                .register(consumerAdv, id(id));
+    }
+
     private TranslationTextComponent trans(String str) {
         return new TranslationTextComponent("mmorpg.advancements." + str);
     }
